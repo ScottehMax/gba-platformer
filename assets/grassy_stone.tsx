8f1aa19471d297fd586a081023c5234a4f10bc91
Scott Maxwell <scott@scotteh.me>
2026-02-16 18:48:37 +0000

    Tile tweak

--- assets/grassy_stone.tsx
+++ assets/grassy_stone.tsx
@@ -12,10 +12,10 @@
    <wangtile tileid="11" wangid="1,1,1,1,1,0,0,0"/>
    <wangtile tileid="12" wangid="1,1,1,1,1,1,1,1"/>
    <wangtile tileid="13" wangid="1,0,0,0,1,1,1,1"/>
-   <wangtile tileid="15" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="15" wangid="1,1,1,0,1,0,0,0"/>
    <wangtile tileid="16" wangid="1,1,1,0,1,1,1,1"/>
    <wangtile tileid="17" wangid="1,1,1,1,1,0,1,1"/>
-   <wangtile tileid="18" wangid="1,0,0,0,0,0,1,1"/>
+   <wangtile tileid="18" wangid="1,0,0,0,1,0,1,1"/>
    <wangtile tileid="20" wangid="1,1,1,0,1,1,1,0"/>
    <wangtile tileid="22" wangid="1,1,1,0,0,0,0,0"/>
    <wangtile tileid="23" wangid="1,1,1,0,0,0,1,1"/>
